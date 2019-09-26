a85c15c29853c419af197aa81218e5e951dd8d97
Роман <p314154265@gmail.com>
2019-09-26 16:29:28 +0300

    Add Player Class v1

---
+++ GameModel/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameModel
+{
+    class Player
+    {
+        public int Health { get; set; }
+        public String name { get; set; }
+
+    
+    }
+}
